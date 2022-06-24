38ded80cf765038674da1a53caa9088b48ff8fb9
Peter Vereshagin <peter@vereshagin.org>
2022-06-24 09:19:51 +0000

    fix: :test_tube: fail correctly on test

--- cmd/muxing/routes-and-handlers.go
+++ cmd/muxing/routes-and-handlers.go
@@ -16,14 +16,10 @@ func sumAandB(w http.ResponseWriter, r *http.Request) {
 }
 
 func bodyMessage(w http.ResponseWriter, r *http.Request) {
-	a, _ := strconv.Atoi(r.Header["A"][0])
-	b, _ := strconv.Atoi(r.Header["B"][0])
-	c := strconv.Itoa(a + b)
-	w.Header()["a+b"] = []string{c}
-	w.WriteHeader(http.StatusOK)
 }
 
 func addRoutes(router *mux.Router) {
 	router.HandleFunc("/headers", sumAandB).Methods("POST").
 		Headers("a", "", "b", "")
+	router.HandleFunc("/data", sumAandB).Methods("POST")
 }
